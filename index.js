494a7e60f62a26f65f4d415af486f82d930abaa4
hidace <dacewillmott@gmail.com>
2017-03-12 11:50:21 -0400

    test for basic functioning on AWS Lambda

--- index.js
+++ index.js
@@ -4,32 +4,34 @@
 const alexa = require('alexa-app');
 const fetch = require('node-fetch');
 const parser = require('xml2json');
-const https = require('https');
 
+const app = new alexa.app('passport');
 
-const app = new alexa.app('safe-travels');
-
-app.launch((request, response) => {
-  response.say('Welcome to Safe Travels, where you can get info on current travel warnings and alerts issued by the U.S. State Department. Please, ask away...');
+app.launch(function(request, response) {
+  response.say('Welcome to Passport, where you can get info on current travel warnings and alerts issued by the U.S. State Department. Please, ask away...');
+  // fetch('https://travel.state.gov/_res/rss/TWs.xml')
+  //   .then(res => res.text())
+  //   .then(data => JSON.parse(parser.toJson(data)))
+  //   .then(dataObj => {
+  //     const advisoryCount = dataObj.rss.channel.item.length;
+  //     response.say(`There are currently ${advisoryCount} Travel Alerts.`);
+  //   })
+  //   .catch(err => {
+  //     response.say('Sorry, but there appears to have been the following error when trying to process your request:', err);
+  //   });
 });
 
-https.get('https://travel.state.gov/_res/rss/TAs.xml', function(res) {
-  res.on('data', chunk => {
-    const data = parser.toJson(chunk);
-    const dataObj = JSON.parse(data);
-    const description = dataObj.rss.channel.description;
-    const alerts = dataObj.rss.channel.item;
-
-    console.log(`There are currently ${alerts.length} Travel Alerts.`);
-
-    alerts.forEach(function(item, index) {
-      console.log(`Travel alert number ${index + 1} is: ${item.title.replace(/ Travel Alert */g, "")}. It was published on ${item.pubDate}.`);
-      // console.log(item.description.replace(/ *\<[^)]*\> */g, ""));
-    });
-  });
-  // res.err('end', () => {
-  //
-  // })
-});
+// fetch('https://travel.state.gov/_res/rss/TAs.xml')
+//   .then(res => res.text())
+//   .then(data => JSON.parse(parser.toJson(data)))
+//   .then(dataObj => {
+//     const description = dataObj.rss.channel.description;
+//     const alerts = dataObj.rss.channel.item;
+//     console.log(`There are currently ${alerts.length} Travel Alerts.`);
+//     alerts.forEach(function(item, index) {
+//       console.log(`Travel alert ${index + 1} relates to ${item.title.replace(/ Travel Alert */g, "")}. It was issued on ${item.pubDate}.`);
+//       // console.log(item.description.replace(/ *\<[^)]*\> */g, "").replace(/ *\&[^)]*\; */g, " "));
+//     });
+//   });
 
 exports.handler = app.lambda();
