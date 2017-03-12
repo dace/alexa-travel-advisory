0f5d8bb8148fa4ca6bd6debc6ee5972cc92df851
hidace <dacewillmott@gmail.com>
2017-03-12 15:13:04 -0400

    format incoming data

--- index.js
+++ index.js
@@ -30,23 +30,58 @@ app.launch(function(request, response) {
 //     console.log(`There are currently ${alerts.length} Travel Alerts.`);
 //     alerts.forEach(function(item, index) {
 //       console.log(`Travel alert ${index + 1} relates to ${item.title.replace(/ Travel Alert */g, "")}. It was issued on ${item.pubDate}.`);
-//       // console.log(item.description.replace(/ *\<[^)]*\> */g, "").replace(/ *\&[^)]*\; */g, " "));
+//       // console.log(item.description);
 //     });
 //   });
 
-const requestUrl = 'https://travel.state.gov/_res/rss/TWs.xml';
+const warningsURL = 'https://travel.state.gov/_res/rss/TWs.xml';
+const alertsURL = 'https://travel.state.gov/_res/rss/TAs.xml';
 
-const sendRequest = (requestUrl, callback) => {
-  fetch(requestUrl)
+app.intent('GetCountryStatus', {
+  slots: {
+    country: 'AMAZON.Country',
+  },
+  utterances: [
+    'is it safe in {-|country}',
+  ]
+}, (request, response) => {
+
+  const phrase = request.slot('country');
+
+  fetch(warningsURL)
     .then(res => res.text())
     .then(data => JSON.parse(parser.toJson(data)))
     .then(dataObj => {
-      const advisoryDescription = dataObj.rss.channel.description;
+
+      const {description} = dataObj.rss.channel;
       const advisories = dataObj.rss.channel.item;
-      callback;
+
+      let countryStatus = {
+        type: description,
+        country: null,
+        issuedAt: null,
+        advisory: null,
+      };
+
+      advisories.map(item => {
+        const country = item.title.replace(/ Travel Alert */g, "").replace(/ Travel Warning */g, "").toLowerCase();
+        const setPhrase = phrase.toLowerCase();
+        if (country === setPhrase) {
+          countryStatus.country = country;
+          countryStatus.issuedAt = item.pubDate;
+          countryStatus.advisory = item.description;
+        }
+      });
+
+      return countryStatus;
     })
-    .catch(err => {
-      response.say('Sorry, but there appears to have been the following error when trying to process your request:', err);
-    });
-}
+    .then(statusObj => {
+      if (statusObj.country) {
+        response.say(`There is currently a travel warning in effect for ${statusObj.country}. ${statusObj.type} This travel warning was issued at ${statusObj.issuedAt}. ${statusObj.advisory}.`);
+      } else {
+        response.say(`I couldn\'t find a current travel warning for ${phrase} at the moment.`);
+      }
+    })
+});
+
 exports.handler = app.lambda();
