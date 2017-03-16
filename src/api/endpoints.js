77f194d18f7d9999e56102ecd4c245518eb7400e
hidace <dacewillmott@gmail.com>
2017-03-16 01:26:44 -0400

    organize files

---
+++ src/api/endpoints.js
@@ -0,0 +1,7 @@
+const warningsURL = 'https://travel.state.gov/_res/rss/TWs.xml';
+const alertsURL = 'https://travel.state.gov/_res/rss/TAs.xml';
+
+module.exports = {
+  warningsURL,
+  alertsURL,
+};
---
+++ src/model-data.js
@@ -0,0 +1,32 @@
+const parser = require('xml2json');
+
+const modelData = (dataObj) => {
+
+  const notices = {
+    warnings: {},
+    alerts: {},
+  };
+
+  dataObj.forEach((item) => {
+    const advisory = JSON.parse(parser.toJson(item)).rss.channel;
+    const type = advisory.title.toLowerCase().includes('warning') ? 'warnings' : 'alerts';
+    notices[type].title = `travel ${type}`;
+    notices[type].description = advisory.description;
+    notices[type].list = [];
+    advisory.item.forEach((detail) => {
+      notices[type].list.push({
+        title: detail.title,
+        issuedAt: detail.pubDate,
+        description: detail.description
+                      .replace(/\\n/g, ' ')
+                      .replace(/\<.*?\>\s?/g, ' ')
+                      .replace(/\&.*?\;\s?/g, ' '),
+      });
+    });
+  });
+  return notices;
+};
+
+module.exports = {
+  modelData,
+};
