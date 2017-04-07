f1c33b731cb823a6b7ecca2c2befae3debc23a62
Dace Willmott <dacewillmott@gmail.com>
2017-04-07 03:01:28 -0400

    latest build

---
+++ public/src/helpers/get-data.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch');
+
+const getData = url => {
+  return new Promise((resolve, reject) => {
+    fetch(url).then(res => {
+      res.status === 200 ? resolve(res.text()) : reject(res.status);
+    });
+  });
+};
+
+module.exports = { getData };
